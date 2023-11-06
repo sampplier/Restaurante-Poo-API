e88918f4178f9c45bf168458ecfc625f371c915b
Samuel Rodrigues Mendes <samuel.rodrigues@dcx.ufpb.br>
2023-11-06 01:21:12 -0300

    Update RestauranteController.java

--- src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
+++ src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
