80a42dc615f21583327e64535b07f04d5a26ff3c
sampplier <samuel.rodrigues@dcx.ufpb.br>
2023-11-06 20:09:32 -0300

    chocotone

--- src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
+++ src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
@@ -12,11 +12,11 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/restaurante",produces = MediaType.APPLICATION_JSON_VALUE)
-@CrossOrigin(origins = {"http://localhost:3000", "https://front-poo-five.vercel.app", "http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://front-poo-five.vercel.app", "http://localhost:8080"})
 public class RestauranteController {
     @Autowired
     private RestauranteFacade restauranteFacade;
-    @PostMapping
+    @PostMapping("/post")
     @ResponseBody
     public RestauranteDTO criar(@RequestBody RestauranteDTO restauranteDTO){
         return restauranteFacade.criar(restauranteDTO);
@@ -26,7 +26,7 @@ public class RestauranteController {
     public RestauranteDTO atualizar(@PathVariable("restauranteId") Long restauranteId, @RequestBody RestauranteDTO restauranteDTO){
         return restauranteFacade.atualizar(restauranteDTO, restauranteId);
     }
-    @GetMapping
+    @GetMapping("pratos")
     @ResponseBody
     public List<RestauranteDTO> getAll(){
         return restauranteFacade.getAll();
