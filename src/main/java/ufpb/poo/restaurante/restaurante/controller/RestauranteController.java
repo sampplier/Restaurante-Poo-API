ac5f7c50eeb0477e22391de1866302bf8855e1d8
sampplier <samuel.rodrigues@dcx.ufpb.br>
2023-11-06 01:32:30 -0300

    chocotone

--- src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
+++ src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/restaurante",produces = MediaType.APPLICATION_JSON_VALUE)
-
+@CrossOrigin(origins = {"http://localhost:3000", "https://front-poo-five.vercel.app"})
 public class RestauranteController {
     @Autowired
     private RestauranteFacade restauranteFacade;
