f2d36492ce580c50f46e45224dc526bac5b6391a
Samuel Rodrigues Mendes <samuel.rodrigues@dcx.ufpb.br>
2023-11-06 01:15:43 -0300

    Update RestauranteController.java

--- src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
+++ src/main/java/ufpb/poo/restaurante/restaurante/controller/RestauranteController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin
 
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000");
 @RequestMapping(value = "/restaurante",produces = MediaType.APPLICATION_JSON_VALUE)
 
 public class RestauranteController {
